8d11d825a1509091313e82dbea8b5d37845b957e
Raphael Fleury <raphael.fleury9@gmail.com>
2021-05-31 23:41:06 -0300

    Update Cliente

--- src/Handler/Cliente.hs
+++ src/Handler/Cliente.hs
@@ -8,17 +8,21 @@ module Handler.Cliente where
 
 import Import
 import Text.Cassius
-import Text.Lucius
- 
-formCliente :: Form Cliente
-formCliente = renderDivs $ Cliente
-    <$> areq textField "Nome: "  Nothing
-    <*> areq textField "CPF:  "  Nothing
-    <*> areq intField  "Idade: " Nothing
+
+-- formCliente :: Form Cliente
+-- formCliente = renderDivs $ Cliente
+--     <$> areq textField "Nome: "  Nothing
+--     <*> areq textField "CPF:  "  Nothing
+--     <*> areq intField  "Idade: " Nothing
+formCliente :: Maybe Cliente -> Form Cliente
+formCliente mc = renderDivs $ Cliente
+    <$> areq textField "Nome: "  (fmap clienteNome mc)
+    <*> areq textField "CPF:  "  (fmap clienteCpf mc)
+    <*> areq intField  "Idade: " (fmap clienteIdade mc)
     
 getClienteR :: Handler Html
 getClienteR = do
-    (widget,_) <- generateFormPost formCliente
+    (widget,_) <- generateFormPost (formCliente Nothing)
     msg <- getMessage
     defaultLayout $ do
         addStylesheet (StaticR css_bootstrap_css)
@@ -28,7 +32,7 @@ getClienteR = do
 
 getCadastroR :: Handler Html
 getCadastroR = do
-    (widget,_) <- generateFormPost formCliente
+    (widget,_) <- generateFormPost (formCliente Nothing)
     msg <- getMessage
     defaultLayout $ do
         addStylesheet (StaticR css_bootstrap_css)
@@ -38,7 +42,7 @@ getCadastroR = do
 
 postClienteR :: Handler Html
 postClienteR = do
-    ((result,_),_) <- runFormPost formCliente
+    ((result,_),_) <- runFormPost (formCliente Nothing)
     case result of
         FormSuccess cliente -> do
             runDB $ insert cliente
@@ -66,7 +70,27 @@ getClientesPageR = do
 
         $(whamletFile "templates/Clientes.hamlet")
 
+getEditarCliR :: ClienteId -> Handler Html
+getEditarCliR cid = do
+    cliente <- runDB $ get404 cid
+    (widget,_) <- generateFormPost (formCliente (Just cliente))
+    msg <- getMessage
+    defaultLayout $ (formWidget "Editar" widget (EditarCliR cid) msg)
+
+postEditarCliR :: ClienteId -> Handler Html
+postEditarCliR cid = do
+    clienteAntigo <- runDB $ get404 cid
+    ((result,_),_) <- runFormPost (formCliente Nothing)
+    case result of
+        FormSuccess novoCliente -> do
+            runDB $ replace cid novoCliente
+            redirect ClientesPageR
+        _ -> redirect ClientesPageR
+
 postApagarCliR :: ClienteId -> Handler Html
 postApagarCliR cid = do
     runDB $ delete cid
     redirect ClientesPageR
+
+formWidget :: Text -> Widget -> Route App -> Maybe Html -> Widget
+formWidget title widget route errorMsg = $(whamletFile "templates/ClienteForm.hamlet")
